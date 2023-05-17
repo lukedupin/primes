a68ac061e81ff57d224d93b0a848045bc7a95ad4
Luke Dupin <lukedupin@gmail.com>
2023-05-17 11:00:48 -0700

    Insights

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # primes
+
+Interesting findings.
+
+Everything repeats at an interval of either
+
+Length = 
+6^2 * x^2 -1 or
+(6*x - 1) * (6*x + 1)
+
++-1 will always overlap on the 6x-1 line at: 6*x^2
++-1 will always overlap on the 6x+1 line at: Length - 6*x^2
+
+From the +-1 overlap, the center of expanding +- interactions is always 6*x interval
+
+The expansion of overlap increases by 1 step in both positive and negative directions: +.- then +...- then +.....-
+There is a point where The +.X.- will be touching another plus. The feature is always +.x.-+....+- and then reverses.
+
+The gap between -+ and then next +- is always 6*(x-1)+4 or 6x-2
+
+The 6x+1 and -1 always have an interaction of .+ and .- as the start and the opposite at the end - and +. The gap between these is x.
--- mapper_tree.cpp
+++ mapper_tree.cpp
@@ -35,27 +35,28 @@ int main()
     int hit;
     char c[] = {'.', '+', '-', '*'};
 
-    const int x = 2;
+    const int x = 3;
     const int x_min = 6 * x - 1;
     const int x_max = 6 * x + 1;
     const int len = x_max * x_min;
 
     //Setup my buffer
     char* buf[] = {
-        new char[len + 1],
-        new char[len + 1],
+        new char[len + 1+100],
+        new char[len + 1+100],
     };
     buf[0][len] = buf[1][len] = 0;
 
     printf("Len %d\n", len); // Awlays equal to 6^2 * x^2 - 1
 
     //Check my map
+    int o = 0;
     for ( i = 0; i < len; i++ )
     {
         hit = 0;
         if ( (6 * i - 1) % x_min == 0 ) hit |= 1;
         if ( (6 * i - 1) % x_max == 0 ) hit |= 2;
-        buf[0][i] = c[hit];
+        buf[0][i+o] = c[hit];
         if ( hit == 3 ) {
             //Always hits at i^2 * 6
             printf("X-1 double => %d\n", i);
@@ -64,11 +65,18 @@ int main()
         hit = 0;
         if ( (6 * i + 1) % x_min == 0 ) hit |= 1;
         if ( (6 * i + 1) % x_max == 0 ) hit |= 2;
-        buf[1][i] = c[hit];
+        buf[1][i+o] = c[hit];
         if ( hit == 3 ) {
             //Always = len - i^2 * 6
             printf("X+1 double => %d\n", i);
         }
+
+        if ( false && i > 0 && i % 6 == 0 ) {
+            o++;
+            buf[0][i+o] = buf[1][i+o] = '|';
+        }
+
+        buf[0][i+o+1] = buf[1][i+o+1] = 0;
     }
 
     //Print out my info
@@ -79,12 +87,12 @@ int main()
     const int start = 6 * x * x;
     for ( i = 0; i < 6 * x - 1; i++ )
     {
-        const int i2 = i * 2;
-        for ( int j = 6; j >= 1; j--) {
+        const int i2 = i * x;
+        for ( int j = 3; j >= 1; j--) {
             printf("%c", buf[0][i2*6 + start - j]);
         }
         printf("%c", buf[0][i2*6 + start]);
-        for ( int j = 1; j < 6; j++) {
+        for ( int j = 1; j <= 2; j++) {
             printf("%c", buf[0][(i2 * 6 + start + j) % len]);
         }
         printf("\n");
